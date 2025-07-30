4be7aa3105cd6d9de677bdcc567b4f81acda518a
汪明杰 <1209497940@qq.com>
2025-07-30 22:49:53 +0800

    add code

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     unoptimized: true
   },
   basePath: process.env.NODE_ENV === 'production' ? '/resume' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/resume/' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/resume/' : '',
 }
 
 module.exports = nextConfig
